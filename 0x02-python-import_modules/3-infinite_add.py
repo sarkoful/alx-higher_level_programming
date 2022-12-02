a3adf3f1618e91726a0ad9384f7624703f47f268
sarkoful <dejireh90@gmail.com>
2022-12-01 18:06:12 -0800

    The boy

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    from sys import argv as argvec
+    add_ = 0
+    counter = 1
+    while counter != len(argvec):
+        add_ += int(argvec[counter])
+        counter += 1
+    print('{}'.format(add_))
