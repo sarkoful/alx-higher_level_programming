96bcca73fb7c0e64559bb0d4b5b85abb4bcf4d1c
sarkoful <dejireh90@gmail.com>
2022-12-07 23:49:40 +0000

    Name them

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    sum_ = 0
-    for i in set(my_lisst):
-        sum_ += i
-    return sum_
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
