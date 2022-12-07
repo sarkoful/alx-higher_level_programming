1253985f303291b86996b1a4539ad4850ed1c125
sarkoful <dejireh90@gmail.com>
2022-12-07 23:54:30 +0000

    Kame

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for index, value in enumerate(my_list):
-        if value == search:
-            my_list[index] = replace
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
